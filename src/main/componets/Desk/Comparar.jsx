58a226373d355b547ee7d4373dc4a03140f90096
torresvictor100 <torresvictor100@gmail.com>
2023-11-12 20:50:24 -0300

    colocando para fucionar o grafico

--- src/main/componets/Desk/Comparar.jsx
+++ src/main/componets/Desk/Comparar.jsx
@@ -57,9 +57,9 @@ const Comparar = () => {
 
   useEffect(() => {
     const Piloto1BaseUrl =
-      "http://ergast.com/api/f1/2021/drivers/max_verstappen/results.json";
+      "http://ergast.com/api/f1/2020/drivers/max_verstappen/results.json";
     const Piloto2BaseUrl =
-      "http://ergast.com/api/f1/2021/drivers/hamilton/results.json";
+      "http://ergast.com/api/f1/2020/drivers/hamilton/results.json";
 
     Promise.all([axios(Piloto1BaseUrl), axios(Piloto2BaseUrl)])
       .then((responses) => {
@@ -79,73 +79,16 @@ const Comparar = () => {
       });
   }, []); 
 
-  const setResults = (results) => {
-    return results.map((result) => ({
-      name: result.Constructor.name,
-      nationality: result.Constructor.nationality,
-    }));
-  };
-
   const renderTable = () => {
     return <Table pilotoRaces1={state.Pilot1List} pilotoRaces2={state.Pilot2List} />;
   };
 
 
   const renderGrafic = () => {
-    return <Grafic options={state.options} data={getGraficData()} />;
+    return <Grafic options={state.options} pilotoRaces1={state.Pilot1List} pilotoRaces2={state.Pilot2List} />;
   };
-
-  const getRacesName = (pilotoRaces1, pilotoRaces2 ) => {
-
-    pilotoRaces1.forEach((races) => {
-      if (!state.labels.includes(races.raceName)) {
-        state.labels.push(races.raceName);
-      }
-    });
-
-    pilotoRaces2.forEach((races) => {
-      if (!state.labels.includes(races.raceName)) {
-        state.labels.push(races.raceName);
-      }
-    });
-
-    return state.labels;
-  };
-
-  const getRacesPotuacao = (pilotoRaces1, labels) => {
-    let potuacao = []
-    pilotoRaces1.forEach((races) => {
-      if (!labels.includes(labels.raceName)) {
-        potuacao.push(parseInt(races.Results[0].points))
-      }
-      else{
-        potuacao.push(parseInt(0))
-      }
-    });
-   return potuacao
-  }
   
 
-  const getGraficData = () => {
-    let labels = getRacesName(state.Pilot1List, state.Pilot2List);
-    getRacesPotuacao(state.Pilot1List,labels )
-    let datasets = [
-      {
-        label: "label",
-        data: getRacesPotuacao(state.Pilot1List, labels),
-        backgroundColor: "#ff0000",
-      },
-      {
-        label: "label",
-        data: getRacesPotuacao(state.Pilot2List, labels),
-        backgroundColor: "#62615d",
-      },
-    ];
-    return {
-      labels,
-      datasets,
-    };
-  };
 
   return <Main {...headerProps}>{renderTable()}{renderGrafic()}</Main>;
 };
