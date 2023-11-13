49935eb0c7951fa884c49221c7b8c259aa8c5baa
torresvictor100 <torresvictor100@gmail.com>
2023-11-13 08:50:18 -0300

    refaction

--- src/main/componets/Graphic/Graphic.jsx
+++ src/main/componets/Graphic/Graphic.jsx
@@ -38,15 +38,11 @@ const Graphic = ({ options, pilotRaces1 , pilotRaces2 }) => {
   const getLabelPilot = (races) => {
     return races.Results[0].Driver.familyName
   }
-  //esse metodo a baixo ta bugado ta esta pulando as corridas que o cara não correu
-  //o erro esta acontecendo pq ta fazendo o for das corridas que o piloto participou e na verdade
-  //tem de ser das lebels
+
   const getRacesScore = (pilotRaces, labels) => {
     let racesScore = []
     let momentScore = 0
     let corrida: Record<string, string> = {}
-
-    
 
     pilotRaces.forEach((races) => {
     corrida[races.raceName] = races.Results[0].points;
